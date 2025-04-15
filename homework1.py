adef9b1d185f1c3f77614a5bbe2e927ba0e6f2b9
Ayushmaan Puri <aypuri@ucsd.edu>
2025-04-14 20:09:42 -0700

    q6

--- homework1.py
+++ homework1.py
@@ -1,14 +1,14 @@
 import numpy as np
 from scipy.io import wavfile
 import glob
-# from mido import MidiFile
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.metrics import classification_report
-# import math
+from mido import MidiFile
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+import math
 
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 SAMPLE_RATE = 44100
 
@@ -86,3 +86,40 @@ def create_sawtooth_wave(frequency, duration, sample_rate=44100):
         wave += ((-1)**(k+1) / k) * np.sin(2 * np.pi * k * frequency * t)
     wave *= (2 / np.pi)
     return wave
+
+def get_file_lists():
+    piano_files = sorted(glob.glob("./piano/*.mid"))
+    drum_files = sorted(glob.glob("./drums/*.mid"))
+    return piano_files, drum_files
+
+def get_num_beats(file_path):
+    # Q6: Your code goes here
+    mid = MidiFile(file_path)
+    # Might need: mid.tracks, msg.time, mid.ticks_per_beat
+    total_ticks = 0
+    # calculate the cumulative delta time for each track and choose the maximum
+    for track in mid.tracks:
+        track_ticks = sum(msg.time for msg in track)
+        if track_ticks > total_ticks:
+            total_ticks = track_ticks
+    # Number of beats (ensure ticks_per_beat is non-zero)
+    nBeats = 0
+    
+    if mid.ticks_per_beat:
+        nBeats = total_ticks / mid.ticks_per_beat
+
+    return nBeats
+
+def get_stats(piano_path_list, drum_path_list):
+    piano_beat_nums = []
+    drum_beat_nums = []
+    for file_path in piano_path_list:
+        piano_beat_nums.append(get_num_beats(file_path))
+        
+    for file_path in drum_path_list:
+        drum_beat_nums.append(get_num_beats(file_path))
+    
+    return {"piano_midi_num":len(piano_path_list),
+            "drum_midi_num":len(drum_path_list),
+            "average_piano_beat_num":np.average(piano_beat_nums),
+            "average_drum_beat_num":np.average(drum_beat_nums)}
